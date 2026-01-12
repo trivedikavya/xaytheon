b9a36733736359eb2c878da5cbc898080b685181
SavajDrashti <savajdrashti2511@gmail.com>
2026-01-12 23:23:45 +0530

    Fix login failure by initializing missing SQLite columns

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,6 +1,9 @@
 const sqlite3 = require("sqlite3").verbose();
+const path = require("path");
 
-const db = new sqlite3.Database("./users.db", (err) => {
+const dbPath = path.join(__dirname, "..", "users.db");
+
+const db = new sqlite3.Database(dbPath, (err) => {
   if (err) console.error(err.message);
   else console.log("📦 Connected to SQLite database");
 });
