f7c6ff90cd013f7ac9716dea910e987cf68ca5de
Gowthami mudam <gowthamimudam@gmail.com>
2026-01-17 19:30:11 +0530

    chore: add trend score utility scaffold

---
+++ js/utils/trendScore.js
@@ -0,0 +1,10 @@
+/**
+ * Calculates a trend score for a GitHub repository
+ * based on growth and recency signals.
+ *
+ * @param {Object} repo - GitHub repository object
+ * @returns {number} trendScore
+ */
+export function calculateTrendScore(repo) {
+  return 0;
+}
